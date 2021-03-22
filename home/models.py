df384752d09e9cc094fc26ccd5051c62bd78dc51
Animesh Shrivatri <animesh.shrivatri2002@gmail.com>
2021-03-23 01:21:20 +0530

    adding model of why eduvue

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class WhyEduVueText(models.Model):
+    head_text=models.CharField(max_length=300)  
+    abbrivationtext= models.CharField(max_length=500)
+    display = models.BooleanField()  
+
+    def __str__(self):
+        return self.head_text
+    
--- home/views.py
+++ home/views.py
@@ -1,22 +1,20 @@
 from django.shortcuts import render , redirect
+from .models import WhyEduVueText
+
+def get_wevt_model_data(obj):
+    l=[]
+    l.append(obj.head_text)
+    l.append(obj.abbrivationtext)
+    return l
+    
 
 # Create your views here.
 def home(request):
+    wevt=list(map(get_wevt_model_data,WhyEduVueText.objects.filter(display=True)))
+    print(wevt)    
     home_context={
     	'carousel_text':['Demo1','Demo2','Demo3'],
-    	'why_eduvue_text':[
-    	['Cost','This will cost you less than $9/mo'],
-    	['Support','Support team works all 365 days. We will solve your problem within 24 hours.'],
-    	['Website','Free Website with domain name.'],
-    	['Learning Management System (LMS)','We provide LMS for students for perform all task asign by institution.'],
-    	['Staff and Student Attandance Management','We use latest technology for attandance.'],
-    	['Invoice & Expense Tracking and Management','Automated payment reminders to students, Staff salary disbursal system, Current P&L statement for month year, Expense tracking'],
-    	['Online assessment tool','Plateform for online exam'],
-    	[' Question Bank Management System','Practice Question for students'],
-    	['Mock Test Platform for students','To practise for various exams'],
-    	['Analytic Reporting System','provide student report.'],
-    	['Mobile Application','To access course from anywhere'],
-    	]
+    	'why_eduvue_text':wevt,
     }
     return render(request,'home/home.html',home_context)
 
